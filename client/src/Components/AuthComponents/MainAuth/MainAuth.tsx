ba428ccf0f7979f62264ff6176758c69251296e6
Прасолов Владимир <vladimirprasolov613@gmail.com>
2022-12-08 09:34:10 +0300

    auth-button-bug-fixed

--- client/src/Components/AuthComponents/MainAuth/MainAuth.tsx
+++ client/src/Components/AuthComponents/MainAuth/MainAuth.tsx
@@ -8,17 +8,16 @@ export function MainAuth() {
     const [isLogin, setIsLogin] = React.useState(false);
     const [isRegister, setRegister] = React.useState(true);
 
-    const handleClick = (event: any) => {
-        console.log(event.target.children)
+    const handleClick = () => {
         setIsLogin(!isLogin);
         setRegister(!isRegister);
     }
 
         return <div className="authContainer">
                     <div className="authContainer__button-grp">
-                        <button onClick={handleClick} className="switch-reg-login-button">Регистрация</button>
-                        <button onClick={handleClick} className="switch-reg-login-button">Войти</button>
-                    </div>
+                        <button onClick={handleClick} className="switch-reg-login-button" disabled={isRegister ? true : false}>Регистрация</button>
+                        <button onClick={handleClick} className="switch-reg-login-button" disabled={isLogin ? true : false}>Войти</button>
+                    </div>{/*  На кнопках не сохраняется эффект фокуса, из-за того что переисовыввается компонент? */}
                     <Register isRegister={isRegister} />
                     <Login isLogin={isLogin}/>
               </div>
